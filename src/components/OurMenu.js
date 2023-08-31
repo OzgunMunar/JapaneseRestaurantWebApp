b71c2df50325fe69d6faa740a7a379f9679b637d
Ozgun Munar <ozgunmnr@gmail.com>
2023-08-31 01:08:27 -0400

    menu page is done with everything it needs.

--- src/components/OurMenu.js
+++ src/components/OurMenu.js
@@ -1,25 +1,40 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 import '../styles/menu.css'
 
 const OurMenu = ({ datas }) => {
+
   return (
+
     <div className='our_menu_container'>
 
     {datas?.map((item) => {
 
       return (
-      <div className='our_menu_item'>
-        <img className='our_menu_item_image' src={item.foodImageLink} alt='ss' />
-        <p>{item.foodName}</p>
-        <p>{item.foodIngredients}</p>
-        <p>{item.foodPrice}</p>
-        <p>{item.foodType}</p>
-      </div>
+
+        <div className='our_menu_item' key={nanoid()}>
+
+          <div className='our_menu_item_image_container'>
+              <img className='our_menu_item_image' src={item.foodImageLink} alt='ss' />
+          </div>
+
+          <div className='horizontal_line2'></div>
+
+          <p className='our_menu_food_name'>{item.foodName}</p>
+          <p>{item.foodIngredients}</p>
+          <p>${item.foodPrice}</p>
+          <p>({item.foodType})</p>
+          
+        </div>
+
       )
+
     })}
 
   </div>
+
   )
+
 }
 
 export default OurMenu
--- src/components/routes/Menu.js
+++ src/components/routes/Menu.js
@@ -1,26 +1,58 @@
-import React, { useState, useLayoutEffect } from 'react'
+import React, { useState } from 'react'
 import MenuData from '../../data/data.js'
 import OurMenu from '../OurMenu.js'
+import '../../styles/menu.css'
 
 const Menu = () => {
 
-  const [data, setData] = useState([])
+  const FoodTypes = {
+    All: "All", 
+    MainDish: "Main Dish",
+    Drink: "Drink",
+    Desert: "Desert"
+  }
 
-  useLayoutEffect(() => {
+  const [filteredData, setFilteredData] = useState(MenuData)
+  const [activeButton, setActiveButton] = useState(FoodTypes.All)
 
-    setData(MenuData)
+  const foodTypeChangeHandler = (foodType) => {
 
-  }, [])
+    if(foodType === FoodTypes.All)
+    {
+      setFilteredData(MenuData)
+      setActiveButton(FoodTypes.All)
+    }
+    else {
+      setFilteredData(filteredDatas => {
+        return MenuData.filter((res) => res.foodType === foodType)
+      })
+      setActiveButton(foodType)
+    }
+
+  }
 
   return (
-    <div className='menu_container'>
-      <p>Menu Page</p>
-      {/* <div className='our_menu_'>
 
-      </div> */}
-      <OurMenu datas={data}/>
+    <div className='menu_container'>
+
+      <p className='menu_page_title first_section_title'>Our Menu</p>
+      <p className='menu_page_explanation first_section_explanation'>List of the foods that we cook & serve with love</p>
+
+      <div className='menu_page_food_type_button_list'>
+
+        <button type='button' className = { activeButton === FoodTypes.All ? 'active' : '' } onClick={() => foodTypeChangeHandler(FoodTypes.All)}>All</button>
+        <button type='button' className = { activeButton === FoodTypes.MainDish ? 'active' : '' }  onClick={() => foodTypeChangeHandler(FoodTypes.MainDish)}>Main Dishes</button>
+        <button type='button' className = { activeButton === FoodTypes.Drink ? 'active' : '' }  onClick={() => foodTypeChangeHandler(FoodTypes.Drink)}>Drinks</button>
+        <button type='button' className = { activeButton === FoodTypes.Desert ? 'active' : '' }  onClick={() => foodTypeChangeHandler(FoodTypes.Desert)}>Deserts</button>
+
+      </div>
+
+      <OurMenu datas={filteredData}/>
+
     </div>
+
   )
+
 }
 
 export default Menu
